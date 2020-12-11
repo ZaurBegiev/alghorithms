959f6c79bf627badbdeea45e55e4f20e284d3af5
zaurbek.begiev <zaurbek.begiev@x5.ru>
2020-12-12 01:48:02 +0300

    Merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -24,12 +24,9 @@ def merge_sort(L: List) -> List:
         return merge(left, right)
 
 
-
 case_1 = []
 case_2 = [1]
 case_3 = [1, 3, 54215, 2, 4, 245, 12, 51, 8, 51, 6, 4, 3]
 
 for case in (case_1, case_2, case_3)
     print(merge_sort(case))
-
-
